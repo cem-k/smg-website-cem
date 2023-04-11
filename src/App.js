f75383429291d040f677da545678235eb261791d
Cem Kirciltepeli <cem.kirciltepeli@code.berlin>
2023-04-12 01:25:52 +0200

    hm

--- src/App.js
+++ src/App.js
@@ -5,24 +5,25 @@ import {
   Home, About, Privacy, Terms, Cookies, Return, Disclaimer, AcceptableUse, Eula, GetStarted
 } from './container';
 
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, BrowserRouter as Router} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-            <Routes>
-            <Route exact path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/> 
-
-            <Route path="/privacy" element={<Privacy/>}/> 
-            <Route path="/terms" element={<Terms/>}/> 
-            <Route path="/cookies" element={<Cookies/>}/> 
-            <Route path="/return" element={<Return/>}/> 
-            <Route path="/disclaimer" element={<Disclaimer/>}/> 
-            <Route path="/use" element={<AcceptableUse/>}/> 
-            <Route path="/eula" element={<Eula />}/> 
-            <Route path="/getstarted" element={<GetStarted />}/> 
+      <Router basename="/smg-website-cem">
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/> 
+          <Route path="/privacy" element={<Privacy/>}/> 
+          <Route path="/terms" element={<Terms/>}/> 
+          <Route path="/cookies" element={<Cookies/>}/> 
+          <Route path="/return" element={<Return/>}/> 
+          <Route path="/disclaimer" element={<Disclaimer/>}/> 
+          <Route path="/use" element={<AcceptableUse/>}/> 
+          <Route path="/eula" element={<Eula />}/> 
+          <Route path="/getstarted" element={<GetStarted />}/> 
         </Routes>
+      </Router>
     </div>
   );
 }
