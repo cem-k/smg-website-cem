3416c16e0a378f59f133bb6adb2ba111a89a351a
Cem Kirciltepeli <cem.kirciltepeli@code.berlin>
2023-04-12 00:57:26 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import {
+  Home, About, Privacy, Terms, Cookies, Return, Disclaimer, AcceptableUse, Eula, GetStarted
+} from './container';
+
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/> 
+
+            <Route path="/privacy" element={<Privacy/>}/> 
+            <Route path="/terms" element={<Terms/>}/> 
+            <Route path="/cookies" element={<Cookies/>}/> 
+            <Route path="/return" element={<Return/>}/> 
+            <Route path="/disclaimer" element={<Disclaimer/>}/> 
+            <Route path="/use" element={<AcceptableUse/>}/> 
+            <Route path="/eula" element={<Eula />}/> 
+            <Route path="/getstarted" element={<GetStarted />}/> 
+        </Routes>
     </div>
   );
 }
---
+++ src/container/Legal/AcceptableUse.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+
+const AcceptableUse = () => {
+  return (
+    <div className="legal">
+      <Navbar />
+
+      <h2>Acceptable Use Policy ✔️</h2>
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default AcceptableUse;
